fd303d8099693a68d822f848502c27debc2673f6
Daniel Moroz <daniellebed@campus.technion.ac.il>
2025-03-24 11:58:06 +0200

    deleting RL

--- run_sparse_rs.sh
+++ run_sparse_rs.sh
@@ -11,16 +11,16 @@ export PYTHONPATH=$PYTHONPATH:/home/daniellebed/project/GeoClip_adv
 
 #### geoclip
 
-# untargted geoclip, loss margin:
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm L0 --bs 32 --n_queries 1000 --k 1 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 2 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 4 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 8 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 16 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 32 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 64 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 128 --device cuda
-python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 224 --device cuda
+# untargted geoclip, loss margin - ran:
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm L0 --bs 32 --n_queries 1000 --k 1 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 2 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 4 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 8 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 16 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 32 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 64 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 128 --device cuda
+# python ./sparse_rs/eval.py --loss margin --model geoclip --norm patches --bs 32 --n_queries 1000 --k 224 --device cuda
 
 # untargted geoclip, loss ce:
 python ./sparse_rs/eval.py --loss ce --model geoclip --norm L0 --bs 32 --n_queries 1000 --k 1 --device cuda
--- run_vis.sh
+++ run_vis.sh
@@ -1,11 +1,9 @@
-data=results
-attack=sparse_rs_L0
-name=sparse_rs_L0_clip_1_200_nqueries_100_pinit_0.30_loss_margin_eps_400_targeted_False_targetclass_None_seed_42.pth
-
-pdf_outputname=sparse_rs_pixel_clip_eps400_nqueries_100
+data=results/new
+attack=sparse_rs_patches
+name=sparse_rs_patches_geoclip_1_200_nqueries_1000_pinit_0.30_loss_margin_eps_128_targeted_False_targetclass_None_seed_42
 
 mamba activate cs236207
 export PYTHONPATH=$PYTHONPATH:/home/daniellebed/project/GeoClip_adv
 
 # if geoclip, use --unnormalize
-python ./sparse_rs/vis.py --adv_pth $data/$attack/$name --out_pdf ./results/plots/$pdf_outputname.pdf
+python ./sparse_rs/vis.py --adv_pth $data/$attack/$name.pth --out_pdf ./results/plots/$name.pdf
